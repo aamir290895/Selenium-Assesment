8766583bfaf109f85f322657df27d19a7a14091f
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-22 21:16:30 +0530

    updated

---
+++ SeleniumTestingOne/src/test/java/com/selenium/testing/SeleniumTestingOne/MouseAction.java
@@ -0,0 +1,33 @@
+package com.selenium.testing.SeleniumTestingOne;
+
+import javax.swing.Action;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MouseAction {
+	@Test
+	public static void mouseAction()throws InterruptedException{
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.facebook.com/");
+		WebElement element = driver.findElement(By.id("email"));
+		
+		Actions action = new Actions(driver);
+		action.moveToElement(element).perform();
+		
+		Thread.sleep(2000);
+		driver.close();
+		
+	}
+
+}
