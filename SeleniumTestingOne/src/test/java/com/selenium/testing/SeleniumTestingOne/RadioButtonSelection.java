c1f25bd5caf9ac0a0460856b54945c52b0a09403
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-22 19:59:34 +0530

    updated

---
+++ SeleniumTestingOne/src/test/java/com/selenium/testing/SeleniumTestingOne/RadioButtonSelection.java
@@ -0,0 +1,28 @@
+package com.selenium.testing.SeleniumTestingOne;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RadioButtonSelection {
+	
+	
+	@Test
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.irctc.co.in/nget/train-search");
+		
+		Thread.sleep(1000);
+		WebElement radioBtn = driver.findElement(By.xpath("//*[@id=\"divMain\"]/div/app-main-page/div/div/div[1]/div[1]/div[1]/app-jp-input/div/form/div[4]/div/span[2]/label"));
+		radioBtn.click();
+		
+		Thread.sleep(2000);
+		driver.close();
+	}
+
+}
