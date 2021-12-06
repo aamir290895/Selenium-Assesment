877d262fc52fec54b9850c4bf88cff0961a6ffaa
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-12-06 23:20:46 +0530

    06/12/21

--- ApiTesting/src/main/java/com/apitest/nsp/ApiTesting/RestClient.java
+++ ApiTesting/src/main/java/com/apitest/nsp/ApiTesting/RestClient.java
@@ -3,44 +3,104 @@ package com.apitest.nsp.ApiTesting;
 import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import com.jayway.jsonpath.JsonPath;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @Test
 public class RestClient {
+    String pathUrl ="https://gorest.co.in/public/v1/users";
+
 	
 	@Test
-	public void getMethod() throws ClientProtocolException, IOException, JSONException{
+	public void getMethod() throws  IOException, JSONException{
         
-	    String url ="https://gorest.co.in/public/v1/users";
 		         
-		CloseableHttpClient httpClient=HttpClients.createDefault();
-		         
-		         
-		HttpGet request = new HttpGet(url); 
-	    request.addHeader("Content-Type" , "application/json");
+		OkHttpClient httpClient= new OkHttpClient().newBuilder()
+				  .build();	
+		
+		Request request = new Request.Builder()
+				  .url(pathUrl)
+				  .method("GET", null)
+				  .addHeader("Content-Type", "application/json")
+				  .build();
+		Response response = httpClient.newCall(request).execute();
+		int code = response.code();
 	
-		         
-		CloseableHttpResponse response = httpClient.execute(request); 
-		
-		int code = response.getStatusLine().getStatusCode();
-		System.out.println("Response code is" + code);
-		
+		String responseBody = response.body().string();
+		System.out.println("Response code is" + responseBody);
+	
 		
 		Assert.assertEquals(200, code);
-		String responseString = EntityUtils.toString(response.getEntity(),"UTF-8");
-		System.out.println(responseString);
 		
 		
 		 
+	}
+	
+	@Test
+	public void postMethod() throws  IOException, JSONException {
+		
+		
+		 OkHttpClient httpClient= new OkHttpClient().newBuilder()
+				  .build();
+		
+		 MediaType mediaType = MediaType.parse("text/plain");
+		 
+		 
+		 JSONObject json=new JSONObject();
+    	 json.put("id","101");
+		 
+    	 json.put("user_id",201);
+		 json.put("title","neova");
+		 json.put("body","active");
+		 RequestBody body = RequestBody.create(json.toString(), mediaType);
+		 Request request = new Request.Builder()
+		   .url("https://gorest.co.in/public/v1/users")
+		   .method("POST", body)
+		   .build();
+		 Response response = httpClient.newCall(request).execute();
+		 int code = response.code();
+
+		 System.out.println("Response code is" + code);
+		
+			
+		 Assert.assertEquals(401, code);
+			
+	}
+	
+	
+	@Test
+	public void patchMethod() throws  IOException, JSONException {
+		
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("text/plain");
+				 
+				 JSONObject json=new JSONObject();
+		    	 json.put("id","101");
+				 
+		    	 json.put("user_id",201);
+				 json.put("title","neova");
+				 json.put("body","active");
+				 RequestBody body = RequestBody.create(json.toString(), mediaType);
+				Request request = new Request.Builder()
+				  .url("https://gorest.co.in/public/v1/users")
+				  .method("PATCH", body)
+				  .build();
+				Response response = client.newCall(request).execute();
+				 int code = response.code();
+
+				 System.out.println("Response code is" + code);
+				
+					
+				 Assert.assertEquals(200, code);
+		
 	}
 
 }
